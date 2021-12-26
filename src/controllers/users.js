efa813254f709c31f0642a9d0378c5d87fb5a46c
yuliamassel <yuliamassel8@gmail.com>
2021-12-26 12:09:52 -0800

    add fix lagi

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -99,10 +99,10 @@ const login = async (req, res, next) => {
 
     const resultHash = bcrypt.compare(password, user.password);
 
-    if (!resultHash) {
-      next(createError(403, 'sorry you entered wrong password'));
-    } else {
+    if (resultHash) {
       helpers.response(res, null, 200, null, 'nice youve succesfully login');
+    } else {
+      next(createError(403, 'sorry you entered wrong password'));
     }
   } catch (error) {
     console.log(error);
